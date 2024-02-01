a79ff3b5822ed09f4c79fd8e33ddec412822b3d8
Xinyuan Li <xli257@b17.clsp.jhu.edu>
2024-02-01 12:20:31 -0500

    Small fix
    Signed-off-by: Xinyuan Li <xli257@b17.clsp.jhu.edu>

--- art/attacks/evasion/projected_gradient_descent/projected_gradient_descent_pytorch.py
+++ art/attacks/evasion/projected_gradient_descent/projected_gradient_descent_pytorch.py
@@ -174,7 +174,7 @@ class ProjectedGradientDescentPyTorch(ProjectedGradientDescentCommon):
         else:
             dataset = torch.utils.data.TensorDataset(
                 torch.from_numpy(x.astype(ART_NUMPY_DTYPE)),
-                torch.from_numpy(targets.astype(ART_NUMPY_DTYPE)),
+                torch.from_numpy(targets.astype(ART_NUMPY_DTYPE)).long(),
             )
 
         data_loader = torch.utils.data.DataLoader(
